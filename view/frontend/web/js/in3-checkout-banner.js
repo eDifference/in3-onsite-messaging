2ec7efa80bea67590c7c44a2e034e5f6b9dbc07b
ejodie <ejodie@e-difference.nl>
2022-12-08 15:16:47 +0100

    [feat] 26504: WIP Moved css to less, added admin icon, fixed translation and placement on checkout page

---
+++ view/frontend/web/js/in3-checkout-banner.js
@@ -0,0 +1,15 @@
+define(
+    [
+        'ko',
+        'uiComponent'
+    ],
+    function (ko, Component) {
+        "use strict";
+
+        return Component.extend({
+            defaults: {
+                template: 'Edifference_In3OnsiteMessaging/in3-checkout-banner'
+            },
+        });
+    }
+);
